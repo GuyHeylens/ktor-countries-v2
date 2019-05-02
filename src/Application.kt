825631b7727a815ef99bc8300fd0a9e98f3b89ba
Guy Heylens <guy@mosp.be>
2019-05-02 20:13:26 +0200

    Cleaned up code

--- src/Application.kt
+++ src/Application.kt
@@ -1,22 +1,19 @@
 package be.countries
 
-import com.sun.xml.internal.ws.developer.SerializationFeature
-import io.ktor.application.*
+import io.ktor.application.Application
+import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
 import io.ktor.jackson.JacksonConverter
 import io.ktor.jackson.jackson
-import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.Routing
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
 fun Application.module() {
     install(DefaultHeaders)
     install(CallLogging)
